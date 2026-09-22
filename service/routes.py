2e7468f1a76d7ea7fb843ba8bffd6f188ec0d474
Felipe Monteiro <felipemonteiro.silva@outlook.com>
2026-09-22 10:12:11 -0400

    {lists implemented}

--- service/routes.py
+++ service/routes.py
@@ -62,7 +62,16 @@ def create_accounts():
 ######################################################################
 
 # ... place you code here to LIST accounts ...
-
+@app.route("/accounts", methods=["GET"])
+def list_accounts():
+    """Returns a list of Accounts"""
+    app.logger.info("Request to list all Accounts")
+    
+    accounts = Account.all()
+    account_list = [account.serialize() for account in accounts]
+    
+    app.logger.info("Returning [%s] accounts", len(account_list))
+    return jsonify(account_list), status.HTTP_200_OK
 
 ######################################################################
 # READ AN ACCOUNT
--- tests/test_routes.py
+++ tests/test_routes.py
@@ -124,3 +124,11 @@ class TestAccountService(TestCase):
         self.assertEqual(response.status_code, status.HTTP_415_UNSUPPORTED_MEDIA_TYPE)
 
     # ADD YOUR TEST CASES HERE ...
+    
+    def test_get_account_list(self):
+        """It should Get a list of Accounts"""        
+        self._create_accounts(5)
+        response = self.client.get(BASE_URL)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        data = response.get_json()
+        self.assertEqual(len(data), 5)
